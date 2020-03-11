e7b77e49976df0a93963c41dc358ad5364bf950e
maramamoorthy <maramamoorthy@uk222668.uk.deloitte.com>
2020-03-11 15:06:51 +0000

    Second commit

--- src/main/java/productsinformation/GildedRose.java
+++ src/main/java/productsinformation/GildedRose.java
@@ -4,61 +4,81 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
+
 public class GildedRose {
 
 	
 
-	static ArrayList<Item> items = new ArrayList<Item>();
-	static ArrayList<Item> conjureditems = new ArrayList<Item>();
-	static ArrayList<Item> agedBrieitems = new ArrayList<Item>();
-	static ArrayList<Item> sulfurasItems = new ArrayList<Item>();
-	static ArrayList<Item> normalItems = new ArrayList<Item>();
+	public static ArrayList<Item> listofitems = new ArrayList<Item>();
+	public static ArrayList<Item> conjureditems = new ArrayList<Item>();
+	public static ArrayList<Item> agedBrieitems = new ArrayList<Item>();
+	public static ArrayList<Item> sulfurasItems = new ArrayList<Item>();
+	public static ArrayList<Item> normalItems = new ArrayList<Item>();
+	
+	public static ArrayList<Item> inappropriateItems = new ArrayList<Item>();
+	
 	
 	public static void main(String[] args) {
 		
        
 		
-        items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
         
-         updateQuality();
+		listofitems.add(new Item("+5 Dexterity Vest", 10, 20));
+		listofitems.add(new Item("Aged Brie", 2, 0));
+		listofitems.add(new Item("Elixir of the Mongoose", 5, 7));
+		listofitems.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+		listofitems.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+		listofitems.add(new Item("Conjured Mana Cake", 3, 6));
+        
+         updateQuality(listofitems);
          
 
- 		//items = normalItems;
- 		//items = conjureditems;
- 		//items = sulfurasItems;
- 		//items= agedBrieitems;
+ 		//listofitems = normalItems;
+        // listofitems = conjureditems;
+ 		//listofitems = sulfurasItems;
+ 		//listofitems = agedBrieitems;
  		
  		
  		System.out.println("*** List of items ****");
- 		for (int i = 0; i < items.size(); i++) {
+ 		for (int i = 0; i < listofitems.size(); i++) {
 
  			System.out.println(
- 					items.get(i).getName() + "   " + items.get(i).getSellIn() + "  " + items.get(i).getQuality());
+ 					listofitems.get(i).getName() + "   " + listofitems.get(i).getSellIn() + "  " + listofitems.get(i).getQuality());
 
  		}
 }
 
  		
- 		public static void updateQuality() {
+ 		public static void updateQuality(ArrayList<Item> items) {
  			
  			for (int i = 0; i < items.size(); i++) {
  				
  				String itemName = items.get(i).getName().replaceAll("[^\\s0-9a-zA-Z+-]","");
+ 				int strlength = itemName.length();
+ 				int itemSellIn = items.get(i).getSellIn();
+ 				int itemQuality =  items.get(i).getQuality();
  				
+ 		if(  ((strlength >0) && ((itemSellIn == 0 && itemQuality>0 )|(itemSellIn > 0 && itemQuality<=50))  )     ){   
+ 				 		
  // sorting sulfuras items as a list				
  			
- 				if((itemName.toLowerCase().contains("sulfuras")) &&  (items.get(i).getSellIn() == 0)) {
+ 				//if((itemName.toLowerCase().contains("sulfuras")) &&  (items.get(i).getSellIn() == 0)) {
+ 				if((itemName.toLowerCase().contains("sulfuras"))) {
+ 					
  				String sulfurasItem= itemName;
  				int sellInSulfuras = items.get(i).getSellIn();
  				int qualityOfSulfuras= items.get(i).getQuality(); 
- 				sulfurasItems.add(new Item(sulfurasItem,sellInSulfuras,qualityOfSulfuras));
+ 				    if(items.get(i).getSellIn() == 0) {sulfurasItems.add(new Item(sulfurasItem,sellInSulfuras,qualityOfSulfuras));}
+ 				//sulfurasItems.add(new Item(sulfurasItem,sellInSulfuras,qualityOfSulfuras));
+ 				    else {inappropriateItems.add(new Item(sulfurasItem,sellInSulfuras,qualityOfSulfuras));}
+ 				
  				}
+ 				
+ 							
+ 				
+ 				
+ 				
  			
  // sorting aged brie items as a list			
  				else if(itemName.toLowerCase().contains("aged brie")){
@@ -66,18 +86,21 @@ public class GildedRose {
  				  int sellInAgedB = items.get(i).getSellIn();
  				  int QualityOfAgedB = items.get(i).getQuality();
  				   if (sellInAgedB == 0) {
- 					QualityOfAgedB = 0;
- 				} else if ( (QualityOfAgedB >=0 && QualityOfAgedB <=50) && (sellInAgedB >= 1 && sellInAgedB <= 5) ) {
- 					QualityOfAgedB = QualityOfAgedB + 3;
- 				} else if ( (QualityOfAgedB >=0 && QualityOfAgedB <=50) && (sellInAgedB > 5 && sellInAgedB <= 10) ) {
- 					QualityOfAgedB = QualityOfAgedB + 2;
- 				} else if( (QualityOfAgedB >=0 && QualityOfAgedB <=50) && (sellInAgedB > 10) ){
+ 					QualityOfAgedB = 0;} 
+ 				     else if ( (QualityOfAgedB >=0 && QualityOfAgedB <=50) && (sellInAgedB >= 1 && sellInAgedB <= 5) ) {
+ 					QualityOfAgedB = QualityOfAgedB + 3;} 
+ 				     else if ( (QualityOfAgedB >=0 && QualityOfAgedB <=50) && (sellInAgedB > 5 && sellInAgedB <= 10) ) {
+ 					QualityOfAgedB = QualityOfAgedB + 2;} 
+ 				     else if( (QualityOfAgedB >=0 && QualityOfAgedB <=50) && (sellInAgedB > 10) ){
  					QualityOfAgedB = QualityOfAgedB + 1;
  				}
  	            
  				agedBrieitems.add(new Item(AgedBItem, sellInAgedB, QualityOfAgedB));
  						
  			}
+ 				
+ 				
+ 				
  		
  // sorting conjured items as a list.			
  				else if(itemName.toLowerCase().contains("conjured")){	
@@ -146,7 +169,9 @@ public class GildedRose {
 			}
 
 		}
-
-	}
-
+ 		
+ 		else {inappropriateItems.add(new Item(items.get(i).getName(),items.get(i).getSellIn(),items.get(i).getQuality()));}
+ 			
+ 		}
+ 		}
 }
---
+++ src/test/java/testproductsinformation/GildedRoseTest.java
@@ -0,0 +1,91 @@
+package testproductsinformation;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import productsinformation.GildedRose;
+
+import productsinformation.Item;
+
+
+public class GildedRoseTest {
+
+	static ArrayList<Item> testlistofitems = new ArrayList<Item>();
+	
+	static ArrayList<Item> testnormalitems = new ArrayList<Item>();
+	static ArrayList<Item> testconjureditems = new ArrayList<Item>();
+	static ArrayList<Item> testagedBrieitems = new ArrayList<Item>();
+	static ArrayList<Item> testsulfurasItems = new ArrayList<Item>();
+	
+	
+			
+		@Test
+	public void testupdate() {
+		GildedRose gr = new GildedRose();
+
+		testlistofitems.add(new Item("Sulfuras@,OxfordCicus London", 10, 20));
+		testlistofitems.add(new Item("Conjured dettol liquid", 12, 30));
+		testlistofitems.add(new Item("+5 Dexterity Vest", 10, 20));
+		testlistofitems.add(new Item("~Aged #Brie", 2, 0));
+		testlistofitems.add(new Item("Elixir of the Mongoose", 5, 7));
+		testlistofitems.add(new Item("@Sulfuras, Hand of Ragnaros", 0, 80));
+		testlistofitems.add(new Item("", 3, 6));
+		testlistofitems.add(new Item("SQL DynamoDB Mongoose", 5, 68));
+		testlistofitems.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+		testlistofitems.add(new Item("Conjured Mana Cake", 3, 6));
+		
+		
+		System.out.println("Before calling updateQuality GildedRose.normalItems.size() = "+GildedRose.normalItems.size());
+		System.out.println("GildedRose.conjureditems.size() = "+GildedRose.conjureditems.size());
+		System.out.println("GildedRose.sulfurasItems.size() = "+GildedRose.sulfurasItems.size());
+		System.out.println("GildedRose.agedBrieitems.size() = "+GildedRose.agedBrieitems.size());
+		System.out.println("GildedRose.inappropriateItems.size() = "+GildedRose.inappropriateItems.size());
+
+	
+		
+// before calling updateQuality method		
+		
+		assertEquals(0,GildedRose.normalItems.size());
+		assertEquals(0,GildedRose.conjureditems.size());
+		assertEquals(0,GildedRose.sulfurasItems.size());
+		assertEquals(0,GildedRose.agedBrieitems.size());
+		assertEquals(0,GildedRose.inappropriateItems.size());
+		
+
+		gr.updateQuality(testlistofitems);
+		
+// after calling updateQuality method
+		
+		System.out.println("GildedRose.normalItems.size() = "+GildedRose.normalItems.size());
+		System.out.println("GildedRose.conjureditems.size() = "+GildedRose.conjureditems.size());
+		System.out.println("GildedRose.sulfurasItems.size() = "+GildedRose.sulfurasItems.size());
+		System.out.println("GildedRose.agedBrieitems.size() = "+GildedRose.agedBrieitems.size());
+		System.out.println("GildedRose.inappropriateItems.size() = "+GildedRose.inappropriateItems.size());
+		
+		
+		testlistofitems = GildedRose.normalItems;
+		//testlistofitems = GildedRose.conjureditems;
+		//testlistofitems = GildedRose.sulfurasItems;
+		//testlistofitems = GildedRose.agedBrieitems;
+		//testlistofitems = GildedRose.inappropriateItems;
+		
+		for(int i = 0; i < testlistofitems.size(); i++) {
+
+ 			System.out.println(
+ 					testlistofitems.get(i).getName() + "   " + testlistofitems.get(i).getSellIn() + "  " + testlistofitems.get(i).getQuality());
+
+ 		}
+		
+		
+		assertEquals(3,GildedRose.normalItems.size());
+		assertEquals(2,GildedRose.conjureditems.size());
+		assertEquals(1,GildedRose.sulfurasItems.size());
+		assertEquals(1,GildedRose.agedBrieitems.size());
+		assertEquals(3,GildedRose.inappropriateItems.size());
+
+		
+	}
+
+}
